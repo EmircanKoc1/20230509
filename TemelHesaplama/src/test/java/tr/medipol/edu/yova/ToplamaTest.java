09229b02504707e1f0d4d528107a5582744ab00b
Emircan <emircandv787@gmail.com>
2023-05-09 21:16:07 +0300

    eklemeler

---
+++ TemelHesaplama/src/test/java/tr/medipol/edu/yova/ToplamaTest.java
@@ -0,0 +1,14 @@
+package tr.medipol.edu.yova;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ToplamaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
