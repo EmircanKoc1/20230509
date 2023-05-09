aa4824e17424702c42ec8d35703b3e28c9850aee
Emircan <emircandv787@gmail.com>
2023-05-09 21:21:51 +0300

    eklemeler

--- TemelHesaplama/src/test/java/tr/medipol/edu/yova/ToplamaTest.java
+++ TemelHesaplama/src/test/java/tr/medipol/edu/yova/ToplamaTest.java
@@ -7,8 +7,30 @@ import org.junit.Test;
 public class ToplamaTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testToplamaIkiPozitif() {
+		int sayi1 = 100;
+		int sayi2 = 2;
+		
+		int gerceksonuc = Toplama.topla(sayi1, sayi2);
+		
+		assertEquals(102,gerceksonuc);
 	}
-
+	@Test
+	public void testToplamaIkiNegatif() {
+		int sayi1 = -100;
+		int sayi2 = -2;
+		
+		int gerceksonuc = Toplama.topla(sayi1, sayi2);
+		
+		assertEquals(-102,gerceksonuc);
+	}
+	@Test
+	public void testToplamaBirNegatifBirPozitif() {
+		int sayi1 = 100;
+		int sayi2 = -2;
+		
+		int gerceksonuc = Toplama.topla(sayi1, sayi2);
+		
+		assertEquals(98,gerceksonuc);
+	}
 }
